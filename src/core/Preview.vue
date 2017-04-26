e8af7334349cd6b57c4cfae284a552bf18df3574
cqhaibin <kxhaibin@163.com>
2017-04-26 21:05:09 +0800

    realize preivew
    Signed-off-by: cqhaibin <kxhaibin@163.com>

--- src/core/Preview.vue
+++ src/core/Preview.vue
@@ -1,13 +1,21 @@
-<template>
-    <div class="preview-wrap">
-
-    </div>
-</template>
 <script>
     export default {
         name: 'Preview',
         computed: {
-            
+            comps:function(){
+                return this.$store.state.components;
+            }
+        },
+        render:function(createElement, cxt){
+            return createElement('div',{
+                'class':{
+                    'preview-wrap': true
+                }
+            },[createElement('ul',this.comps.map(function(data,index){
+                return createElement('li',[createElement(data.type,{
+                    props: data.props
+                }, data.props.text)]); //需要区分：props、solt内容、方法等属性内容
+            }))]);
         }
     }
 </script>
